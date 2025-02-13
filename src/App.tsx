9aa41a560419b7cb706001a896416e91d29ada44
LauraCascallana <lauracascallanaferreira@gmail.com>
2025-02-13 10:37:16 +0100

    categorias

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,44 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Profile from './pages/Profile'
-import UserList from './pages/UserList'
-import Navbar from './components/Navbar'
-import OfferList from './pages/OfferList'
-import OfferDetail from './pages/OfferDetail'
-import OfferForm from './pages/OfferForm'
-import { Toaster } from 'react-hot-toast'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import UserList from "./pages/UserList";
+import Navbar from "./components/Navbar";
+import OffertList from "./pages/OfferList";
+import OffertForm from "./pages/OfferForm";
+import OfferDetail from "./pages/OfferDetail";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import CategoryManager from "./pages/CategoryManager";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-      <Navbar/>
-      <Toaster position='top-center' reverseOrder={false}/>
-      <div className='container mx-auto px-8 py-12'></div>
-        <Routes location={''} >
-          <Route path="/" element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/userList" element={<UserList/>}/>
-          <Route path="/offers" element={<OfferList/>}/>
-          <Route path="/offers/:id" element={<OfferDetail/>}/>
-          <Route path="/offers/new" element={<OfferForm/>}/>
-          <Route path="/offers/edit/:id" element={<OfferForm/>}/>
-        </Routes>
+        <div className="flex flex-col">
+          <Navbar />
+          <Toaster position="top-center" reverseOrder={false} />
+          <div className="flex grow justify-center items-center">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/userList" element={<UserList />} />
+              <Route path="/offers" element={<OffertList />} />
+              <Route path="/offers/:id" element={<OfferDetail />} />
+              <Route path="/offers/new" element={<OffertForm />} />
+              <Route path="/offers/edit/:id" element={<OffertForm />} />
+              <Route path="/categories" element={<CategoryManager />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
